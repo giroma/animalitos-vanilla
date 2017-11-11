a654e8062a540e9d68b79e67f062f22f5ca27453
Argir Argirov <dj.giro@gmail.com>
2017-11-11 15:31:33 -0500

    added esc event listener, and the player and album classes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+const player = new Player()
+const album = new Album()
+
+document.body.addEventListener('keyup', (event => {
+  if (event.keyCode === 27){
+    album.hide()
+  }
+}))
+
 const animalsContainer = document.createElement('section')
 animalsContainer.className = 'animals';
 document.body.appendChild(animalsContainer);
@@ -22,7 +31,8 @@ const createAnimalFigure = (animal) => {
   figure.appendChild(caption)
 
   figure.addEventListener('click', (event) => {
-    console.log(event);
+    player.play(animal.sound)
+    album.show(animal)
   })
 
   return figure;
