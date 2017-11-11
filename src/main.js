15b67014644beb6c39b9479c84a14f7d106c02d0
Argir Argirov <dj.giro@gmail.com>
2017-11-11 14:20:07 -0500

    picture and footers added

--- src/main.js
+++ src/main.js
@@ -5,15 +5,25 @@ document.body.appendChild(animalsContainer);
 //create figure tag for individual animal
 const createAnimalFigure = (animal) => {
   const figure = document.createElement('figure');
-  figure.className = 'animal'
+  figure.className = 'animal';
 
   const background = document.createElement('div')
-  background.className = 'background'
+  background.className = 'background';
   background.style.backgroundImage = `url('${animal.image}')`;
-  figure.appendChild(background)
+  figure.appendChild(background);
+
+  const caption = document.createElement('figcaption');
+  caption.style.backgroundColor = animal.bgColor
+  caption.innerHTML = `
+    <h2>${animal.name}</h2>
+    <img src ="${animal.icon}" alt="${animal.name}"
+    class="animal-icon" />
+  `
+  figure.appendChild(caption)
 
   return figure;
 }
+
 
 animals.forEach(animal => {
   animalsContainer.appendChild(createAnimalFigure(animal))
